dc68a902a3c6711b6c7651026fb32813f1959350
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-14 17:23:11 +0200

    Add the the project components

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => (
+  <section>
+    <h2>Home</h2>
+  </section>
+);
+export default Home;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => (
+  <section>
+    <h2>Navbar</h2>
+  </section>
+);
+
+export default Navbar;
