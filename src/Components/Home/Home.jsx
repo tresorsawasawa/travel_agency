af95a04c55a659640a7c228573ad665ead5b243b
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-15 01:06:30 +0200

    Ignore the .vscode directory and add some styling on the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import HomeVideoBg from '../../assets/videos/Home_bg_video.mp4';
 
 const Home = () => (
-  <section>
-    <h2>Home</h2>
+  <section id="home" className="home">
+    <div className="overlay" />
+
+    <video src={HomeVideoBg} className="HomeVideoBg" muted autoPlay loop type="video.mp4" />
+
+    <div className="homeContent">
+      <div className="textContainer">
+        <span className="smallText">Nos Forfaits</span>
+        <h2 className="homeTitle">Découvrir le monde</h2>
+      </div>
+    </div>
   </section>
 );
 export default Home;
