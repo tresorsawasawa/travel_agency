670a249b92dd603a147e4eff9eb0e18b72da6d50
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-14 22:24:13 +0200

    Add logic to handle the mobile menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,71 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineTravelExplore } from 'react-icons/md';
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { FaBars } from 'react-icons/fa';
+import { TbGridDots } from 'react-icons/tb';
 import { Link } from 'react-scroll';
 
-const Navbar = () => (
-  <header className="header flex">
-    <div className="logoContainer flex clickable">
-      <Link to="home" className="clickable">
-        <h1 className="logo flex">
-          <MdOutlineTravelExplore className="icon" />
-          Voyage.
-        </h1>
-      </Link>
-    </div>
+const Navbar = () => {
+  const navLists = [
+    {
+      id: 1, to: 'home', name: 'Home', class: 'navItem  clickable',
+    },
+    {
+      id: 4, to: 'home', name: 'about', class: 'navItem  clickable',
+    },
+    {
+      id: 2, to: 'home', name: 'News', class: 'navItem  clickable',
+    },
+    {
+      id: 3, to: 'home', name: 'Packages', class: 'navItem  clickable',
+    },
+    {
+      id: 5, to: 'home', name: 'Pages', class: 'navItem  clickable',
+    },
+    {
+      id: 6, to: 'home', name: 'Shop', class: 'navItem  clickable',
+    },
+  ];
 
-    <nav className="navbarContainer">
-      <ul className="navList flex">
-        <li className="navItem  clickable">
-          <Link to="home" smooth span={true.toString()} className="navLink">
-            Home
-          </Link>
-        </li>
-        <li className="navItem  clickable">
-          <Link to="main" smooth span={true.toString()} className="navLink">
-            News
-          </Link>
-        </li>
-        <li className="navItem  clickable">
-          <Link to="main" smooth span={true.toString()} className="navLink">
-            Packages
-          </Link>
-        </li>
-        <li className="navItem  clickable">
-          <Link to="main" smooth span={true.toString()} className="navLink">
-            About
-          </Link>
-        </li>
-        <li className="navItem  clickable">
-          <Link to="home" smooth span={true.toString()} className="navLink">
-            Pages
-          </Link>
-        </li>
-        <li className="navItem  clickable">
-          <Link to="home" smooth span={true.toString()} className="navLink">
-            Shop
-          </Link>
-        </li>
+  const [active, setActive] = useState('navbarContainer');
 
-        <li className="bookBtnContainer clickable">
-          <button type="button" className="btn bookBtn">
-            <Link to="home" smooth span={true.toString()} className="navLink">
-              BOOK NOW
-            </Link>
-          </button>
-        </li>
-      </ul>
+  const showNavMenu = () => setActive('navbarContainer activeNavbar backgroundBlur');
 
-      <div className="closeMenu flex">
-        <AiFillCloseCircle className="icon clickable" />
+  const closeNavMenu = () => setActive('navbarContainer');
+
+  return (
+    <header className="header flex">
+      <div className="logoContainer flex clickable">
+        <Link to="home" className="clickable">
+          <h1 className="logo flex">
+            <MdOutlineTravelExplore className="icon" />
+            Voyage.
+          </h1>
+        </Link>
       </div>
-    </nav>
 
-    <div className="hamburger">
-      <FaBars className="icon" />
-    </div>
-  </header>
-);
+      <nav className={active}>
+        <ul className="navList flex">
+          {navLists.map((navItem) => (
+            <li
+              key={navItem.id}
+              aria-hidden="true"
+              className={navItem.class}
+            >
+              <Link to={navItem.to} onClick={() => closeNavMenu()} smooth span={true.toString()} className="navLink">
+                {navItem.name}
+              </Link>
+            </li>
+          ))}
+
+          <li className="bookBtnContainer clickable">
+            <button type="button" className="btn bookBtn">
+              <Link to="home" smooth span={true.toString()} className="navLink">
+                BOOK NOW
+              </Link>
+            </button>
+          </li>
+        </ul>
+
+        <div
+          aria-hidden="true"
+          onClick={() => closeNavMenu()}
+          className="closeMenu flex"
+        >
+          <AiFillCloseCircle className="icon clickable" />
+        </div>
+      </nav>
+
+      <div aria-hidden="true" onClick={showNavMenu} className="hamburger">
+        <TbGridDots className="icon" />
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
