37a990b7fd0d5ca5b6d403a435caae543d0d7ac6
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-16 20:08:25 +0200

    Add routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Footer from './Components/Footer/Footer';
-import Home from './Components/Home/Home';
-import Main from './Components/Main/Main';
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './Pages/HomePage';
 import Navbar from './Components/Navbar/Navbar';
+import Travels from './Pages/Travels';
+import Travel from './Pages/Travel';
+import About from './Pages/About';
 
 const App = () => (
   <>
     <Navbar />
-    <Home />
-    <Main />
-    <Footer />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/travels" element={<Travels />} />
+      <Route path="/travels/:id" element={<Travel />} />
+    </Routes>
   </>
 );
+
 export default App;
---
+++ src/Pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => (
+  <main>
+    <section>
+      <h2 style={{ marginTop: '5rem', textAlign: 'center' }}>
+        The About page is Under Construction
+      </h2>
+    </section>
+  </main>
+);
+
+export default About;
