6d54c381a238de0ef419bc3ef923d39a122c980d
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-18 17:27:12 +0200

    Add default data of mpst visited location

---
+++ src/Components/Main/data.js
@@ -0,0 +1,76 @@
+import uuid from 'react-uuid';
+import Paris from '../../assets/images/paris.jpg';
+import Doha from '../../assets/images/doha.jpg';
+import Kigali from '../../assets/images/Kigali_city.jpg';
+import Honkong from '../../assets/images/honkong.jpg';
+import Dubai from '../../assets/images/dubai.jpg';
+import Berlin from '../../assets/images/berlin.jpg';
+import Dubai1 from '../../assets/images/Dubai.jpg';
+
+const mostVisitedData = [
+  {
+    id: uuid(),
+    image: Kigali,
+    location: 'Kigali',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+  {
+    id: uuid(),
+    image: Paris,
+    location: 'Paris',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+  {
+    id: uuid(),
+    image: Honkong,
+    location: 'Honkong',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+  {
+    id: uuid(),
+    image: Dubai,
+    location: 'Dubai',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+  {
+    id: uuid(),
+    image: Dubai1,
+    location: 'Dubai',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+  {
+    id: uuid(),
+    image: Berlin,
+    location: 'Berlin',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+  {
+    id: uuid(),
+    image: Doha,
+    location: 'Doha',
+    fees: '$234',
+    grade: 'CULTURAL RELAX',
+    description:
+      'Quae, dolor quas sunt, officiis nihil tempore vitae quo a consectetur esse, natus praesentium cupiditate quidem debitis inventore eius voluptatem! Dolores officia voluptatem quis ex a fugit hic, dignissimos corporis. Distinctio vero et tenetur unde repellendus, similique tempora impedit reprehenderit maxime, neque commodi repudiandae quam veniam suscipit asperiores consequuntur officia quod doloremque tempore itaque dolorem veritatis quas. Perferendis, assumenda sint.',
+  },
+];
+
+export default mostVisitedData;
