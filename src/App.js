bbc42d88f6c601f8f16dd66a2939185c3f76cbc0
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-18 20:32:01 +0200

    Update the application routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './Pages/HomePage';
+import Contacts from './Pages/Contacts';
 import Navbar from './Components/Navbar/Navbar';
 import Travels from './Pages/Travels';
 import Travel from './Pages/Travel';
@@ -13,6 +14,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contacts" element={<Contacts />} />
       <Route path="/travels" element={<Travels />} />
       <Route path="/travels/:id" element={<Travel />} />
     </Routes>
